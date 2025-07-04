f181691605eac18f765c2e91f7699057ba156cd6
purnakc <166592833+purnakc@users.noreply.github.com>
2025-07-04 08:34:26 +0545

    added product

--- Backend/src/models/product.js
+++ Backend/src/models/product.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../database/index");
-
+const express = require("express");
 const product = sequelize.define(
   "products",
   {
