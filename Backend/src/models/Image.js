e067d2d22bc73dd872c643252a1c801704409c7c
purnakc <166592833+purnakc@users.noreply.github.com>
2025-07-04 08:30:48 +0545

    added image section

--- Backend/src/models/Image.js
+++ Backend/src/models/Image.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../database/index");
-
+const express = require("express");
 const Images = sequelize.define("images", {
   id: {
     type: DataTypes.INTEGER,
