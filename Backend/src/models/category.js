f1ef1120dd63b4793c793e170606939e0cbb6d15
purnakc <166592833+purnakc@users.noreply.github.com>
2025-07-04 08:21:55 +0545

    added Category

--- Backend/src/models/category.js
+++ Backend/src/models/category.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../database/index");
-
+const express = require("express");
 const categories = sequelize.define(
   "categories",
   {
